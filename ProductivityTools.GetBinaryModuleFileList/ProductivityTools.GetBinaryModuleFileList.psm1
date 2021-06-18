e0ac4b71e06d21b4bc73aded9736d43f3d603001
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-06-18 21:43:21 +0200

    working version

--- ProductivityTools.GetBinaryModuleFileList/ProductivityTools.GetBinaryModuleFileList.psm1
+++ ProductivityTools.GetBinaryModuleFileList/ProductivityTools.GetBinaryModuleFileList.psm1
@@ -1,13 +1,25 @@
-<#
-	My Function
-#>
+
 function Get-BinaryModuleList {
 
 	[cmdletbinding()]
 	param(
-		[string]$Directory
+		[string]$Directory = (Get-Location).Path
 	)
 
+
 	Write-Verbose "Hello"
+	Write-Verbose "Directory: $Directory"
+
+	$items = Get-ChildItem $Directory
+	$beforeLast=$($items.Length-2)
+	$itemsWithoutLast=$items[0..$beforeLast];
+	$lastElement=$items[-1]
+	Write-Output "FileList=@("
+	
+	foreach ($item in $itemsWithoutLast) {
+		Write-Output "'$($item.Name)',"
+	}
+	Write-Output "'$($lastElement.Name)'"
+	Write-Output ")"
 
 }
--- ProductivityTools.GetBinaryModuleFileList/Runner.ps1
+++ ProductivityTools.GetBinaryModuleFileList/Runner.ps1
@@ -2,3 +2,5 @@ clear
 cd $PSScriptRoot
 Import-Module .\ProductivityTools.GetBinaryModuleFileList.psm1 -Force
 Get-BinaryModuleList -verbose 
+Get-BinaryModuleList -Directory "d:\GitHub\ProductivityTools.TeamManagement.Cmdlet\ProductivityTools.TeamManagement.Cmdlet\bin\Debug\netstandard2.0\" -verbose 
+
